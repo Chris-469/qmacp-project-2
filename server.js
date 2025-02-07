9808f083aab3b03c9ab3339e98f8673d5c50ded5
Chris Cocklin <chris.cocklin@ibm.com>
2025-02-07 10:58:43 +0000

    Project initialisation, created express server, added nodemon for dynamic server restart

---
+++ server.js
@@ -0,0 +1,17 @@
+﻿const express = require('express');
+
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res)=>{
+  res.status(200);
+  res.send("Welcome to root URL of Server");
+});
+
+app.listen(PORT, (error) =>{
+    if(!error)
+        console.log("Server is Successfully Running. Server available at http://localhost:3000 "+ PORT)
+    else
+        console.log("Error occurred, server can't start", error);
+    }
+);
