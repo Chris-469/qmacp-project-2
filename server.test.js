5298ad08fe1da3b931a3607e42f7cddd6d54eede
Chris Cocklin <chris.cocklin@ibm.com>
2025-02-14 16:49:38 +0000

    Fixed remote testing bug, allowing automated tests using axios to send requests to the remote server

--- server.test.js
+++ server.test.js
@@ -1,24 +1,18 @@
 ﻿const path = require('path');
 const axios = require('axios');
+const cookieParser = require('cookie-parser');
 const fs = require('fs');
 const express = require('express');
 const app = express();
 const PORT = 3000;
 
-jest.mock("axios"); // Mock axios
+app.use(cookieParser());
+app.use(express.json()); // Middleware to parse JSON payloads
+app.use(express.urlencoded({ extended: true })); // Middleware to parse URL-encoded payloads
+
 const serverURL = "http://9.20.194.48:3000/"
 const ltpaToken = "LtpaToken2=EOtiPsKmrWQov1Qcx8l9n2igXqcbxHHyjS89nGpV4Rl2XyNWieyeQ8jJw+vZY8QynnBl18pBuS6kIrMiVXEi8jIqfCSBbMFoGmHpYHg1mMKZ4tNIi8hL5VaPk2sPMD0yKCeuLSZ1KVNl99rp1+EMfvNVG8QDrtz5e/iHqxO9mjsVLcIZi1ye82gONC4D5S9lq7zvtX7gXGYghPvyhIe4lvwAmkWc1Pw7iXpwuvM6cVw4INo4OiEdj+kW3nD1hKyBk1q90Ac/tCidit2rIRmONyNYYFXE8weW/3X+W8uYML2pTg7jOZE2igK4GeMNvbmH"
 
-// Since I'm mocking the axios request responses, the config can be anything
-let config = {
-  method: 'get',
-  timeout: 10000,
-  maxBodyLength: Infinity,
-  url: serverURL + 'qm-sysparms',
-  headers: {
-    'Cookie': ltpaToken
-    }
-  }
 
 // describe('POST /authenticate', () => {
 //   it('UR1.1 - should respond with status 200, an informative status message and an LtpaToken2 cookie if valid credentials are used', async () => {
@@ -74,13 +68,56 @@ let config = {
 //   });
 // });
 
-describe('GET dataset API server',  () => {
-  it('UR1.1 - should respond with status 200', async () => {
+describe('Test the qmacp server is running',  () => {
+  it('should respond with status 200', async () => {
 
-   // Execute the request
-   const response = await axios.request(config);
+    let config = {
+      method: 'get',
+      timeout: 10000,
+      maxBodyLength: Infinity,
+      url: serverURL,
+      headers: {
+        'Cookie': ltpaToken
+        }
+      }
 
-    // Check the response
-    expect(response.status).toBe(200);
+    let response;
+
+    try{
+       // execute the request to the server
+       response = await axios.request(config);
+    } catch(e) {
+       console.log("an error occurred");
+       response = e.message;
+       console.log(e);
+    }
+  
+     // Check the response and return ECONNREFUSED if the server is not running
+     expect(response?.status || response).toBe(200);
+  });
+});
+
+describe('Test the POST /authenticate endpoint ',  () => {
+  it('should respond with status 200, an informative message and an LtpaToken2 if valid credentials are used', async () => {
+
+    let config = {
+      method: 'post',
+      timeout: 10000,
+      maxBodyLength: Infinity,
+      url: serverURL + "authenticate",
+      headers: {
+        //'Authorization': 'Basic Q0hSSVNDTzpVcmJhbkMwZDNEM3BsMHkz', 
+        }
+      }
+
+    try{
+       // execute the request to the server
+       const response = await axios.request(config);
+       expect(response.status).toBe(200); 
+    } catch(e) {
+       console.log("An error occurred");
+       const response = e?.status || e.message;
+       expect(response).toBe(200); 
+    }
   });
 });
