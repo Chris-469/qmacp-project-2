c4554afd02a582373bdede7b3d9b64644f4f3d5c
Chris Cocklin <chris.cocklin@ibm.com>
2025-02-15 18:23:57 +0000

    Removed hardcoded LtpaToken from tests and added tests for UR(2)

--- server.test.js
+++ server.test.js
@@ -7,9 +7,9 @@ const app = express();
 const PORT = 3000;
 
 const serverURL = "http://9.20.194.48:3000/"
-const ltpaToken = "LtpaToken2=EOtiPsKmrWQov1Qcx8l9n2igXqcbxHHyjS89nGpV4Rl2XyNWieyeQ8jJw+vZY8QynnBl18pBuS6kIrMiVXEi8jIqfCSBbMFoGmHpYHg1mMKZ4tNIi8hL5VaPk2sPMD0yKCeuLSZ1KVNl99rp1+EMfvNVG8QDrtz5e/iHqxO9mjsVLcIZi1ye82gONC4D5S9lq7zvtX7gXGYghPvyhIe4lvwAmkWc1Pw7iXpwuvM6cVw4INo4OiEdj+kW3nD1hKyBk1q90Ac/tCidit2rIRmONyNYYFXE8weW/3X+W8uYML2pTg7jOZE2igK4GeMNvbmH"
+let ltpaToken2;
 
-describe('Test the qmacp server is running',  () => {
+describe('Test the qmacp server is running', () => {
   it('should respond with status 200', async () => {
 
     let config = {
@@ -17,9 +17,7 @@ describe('Test the qmacp server is running',  () => {
       timeout: 10000,
       maxBodyLength: Infinity,
       url: serverURL,
-      headers: {
-        'Cookie': ltpaToken
-        },
+      headers: {},
       }
 
     let response;
@@ -27,10 +25,10 @@ describe('Test the qmacp server is running',  () => {
     try{
        // execute the request to the server
        response = await axios.request(config);
-    } catch(e) {
-       console.log("an error occurred");
-       response = e.message;
-       console.log(e);
+
+    } catch(error) {
+       console.log("An error occurred in testing: " + error?.status || error.message);
+       response = error.message;
     }
   
      // Check the response and return ECONNREFUSED if the server is not running
@@ -38,8 +36,9 @@ describe('Test the qmacp server is running',  () => {
   });
 });
 
-describe('Test the POST /authenticate endpoint ',  () => {
-  it('should respond with status 400, an informative message and no cookies if no credentials are provided', async () => {
+describe('UR(1) - The API must allow users to authenticate using their z/OSMF credentials',  () => {
+    
+  it('UR(1.1) - should respond with status 200 if valid credentials are used', async () => {
 
     let config = {
       method: 'post',
@@ -47,22 +46,27 @@ describe('Test the POST /authenticate endpoint ',  () => {
       maxBodyLength: Infinity,
       url: serverURL + "authenticate",
       headers: {
-        //'Authorization': 'Basic Q0hSSVNDTzpVcmJhbkMwZDNEM3BsMHkz', 
+        'Authorization': 'Basic Q0hSSVNDTzpVcmJhbkMwZDNEM3BsMHkz', // Valid Credentials
         },
       }
 
     try{
        // execute the request to the server
        const response = await axios.request(config);
-       expect(response.status).toBe(400); 
-    } catch(e) {
-       console.log("An error occurred");
-       const response = e?.status || e.message;
-       expect(response).toBe(400); 
+
+       // set the ltpatoken2 test variable for subsequent tests
+       ltpaToken2 = response.headers['set-cookie'];
+
+       expect(response.status).toBe(200);
+    } catch(error) {
+       console.log("An error occurred in testing: " + error?.status || error.message);
+       const response = error?.status || error.message;
+
+       expect(response).toBe(200);
     }
   });
-  
-  it('should respond with status 200, an informative message and an LtpaToken2 if valid credentials are used', async () => {
+
+  it('UR(1.1) - should respond with an informative message if valid credentials are used', async () => {
 
     let config = {
       method: 'post',
@@ -70,19 +74,96 @@ describe('Test the POST /authenticate endpoint ',  () => {
       maxBodyLength: Infinity,
       url: serverURL + "authenticate",
       headers: {
-        'Authorization': 'Basic Q0hSSVNDTzpVcmJhbkMwZDNEM3BsMHkz', 
+        'Authorization': 'Basic Q0hSSVNDTzpVcmJhbkMwZDNEM3BsMHkz', // Valid Credentials
         },
       }
 
     try{
        // execute the request to the server
        const response = await axios.request(config);
-       expect(response.status).toBe(200); 
+
+       expect(response.statusText).toBe("OK");
     } catch(error) {
-       console.log("An error occurred");
-       console.log(error);
+       console.log("An error occurred in testing: " + error?.status || error.message);
        const response = error?.status || error.message;
+
+       expect(response).toBe(200);
+    }
+  });
+
+  it('UR(1.1) - should respond with an LtpaToken2 if valid credentials are used', async () => {
+
+    let config = {
+      method: 'post',
+      timeout: 10000,
+      maxBodyLength: Infinity,
+      url: serverURL + "authenticate",
+      headers: {
+        'Authorization': 'Basic Q0hSSVNDTzpVcmJhbkMwZDNEM3BsMHkz', // Valid Credentials
+        },
+      }
+
+    try{
+       // execute the request to the server
+       const response = await axios.request(config);
+
+       expect(response.headers["set-cookie"]).toBeDefined();
+    } catch(error) {
+       console.log("An error occurred in testing: " + error?.status || error.message);
+       const response = error?.status || error.message;
+
        expect(response).toBe(200);
     }
   });
 });
+
+describe('UR(2) - Users should be notified if an error occurs when authenticating with z/OSMF. ', () => {
+
+  it('UR(2.1) - should respond with status 401 if invalid credentials are used to authenticate', async () => {
+
+    let config = {
+      method: 'post',
+      timeout: 10000,
+      maxBodyLength: Infinity,
+      url: serverURL + "authenticate",
+      headers: {
+        'Authorization': 'Basic SkVGRkVSRTpVcmJhbkMwZDNEM3BsMHkz', // Invalid credentials
+        },
+      }
+    
+    let response;
+  
+    // execute the request to the server
+    try{
+       response = await axios.request(config);
+
+       expect(response.status).toBe(401);  // throws an assertion error if it fails
+    } catch(error) {
+      console.log("An error occurred");
+      const response = error?.status || error.message;
+      expect(response).toBe(401); 
+    }
+  });
+
+  it('UR(2.2) - should respond with status 400 no credentials are provided', async () => {
+
+    let config = {
+      method: 'post',
+      timeout: 10000,
+      maxBodyLength: Infinity,
+      url: serverURL + "authenticate",
+      headers: {},
+      }
+  
+    // execute the request to the server
+    try{
+       const response = await axios.request(config);
+       expect(response.status).toBe(400); 
+    } catch(error) {
+       console.log("An error occurred");
+       const response = error?.status || error.message;
+       expect(response).toBe(400); 
+    }
+  });
+});
+
